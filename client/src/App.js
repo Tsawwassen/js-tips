ea115512b1e7efe934698f7a0d81200e01f8bdd7
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-15 11:01:41 -0700

    Add async/await examples

--- client/src/App.js
+++ client/src/App.js
@@ -165,11 +165,131 @@ function App() {
     return (`Result ${first + second + third}`);
   }
 
+  ////////////////////////////////////////
+  // Fetch example
+  ////////////////////////////////////////
+  // The main thing to remember is that fetch/then code is made to make async code read like sync code
+
+
+  // 7.0 - Making a fetch request
+const testFetch = () => {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/"; //Prevents an error when doing the fetch request
+  const url = "https://example.com"; //Endpoint
+  fetch(proxyurl + url) // Fetching data from the server
+    .then(contents => console.log("WELCOME!!!")) //This code is run after data has been fetched
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?")) //Catch any errors that could be thrown in the fetch or any then sections
+
+  console.log("hello!!!"); //This log is printed before the WELCOME log
+
+  //The main thread gets to the fetch line, and pushs it onto the callback thread, and then continues on the main thread and lob 'hello'
+  // Once the main thread is done, it will go to the callback thread, fetch data, then log 'welcome'
+}
+  
+  // 7.1 - Making a promise function
+  // Putting the async keyword before the argument brackets will make a function return a promise object
+  const getFruit = async(name) => {
+    const fruit ={
+      pineapple: 11,
+      peach: 22,
+      strawberry: 33
+    }
+
+    // Note, using the async keyword in the arguments does code magic to return a promise object
+    // If we did not have the async keyword, we can still return a promise object
+    //    return Promise.resolve(fruits[name]);
+    return fruit[name];
+
+  }
+
+  //Async + await
+  const makeSmoothie = async() => {
+
+    //Instead of using multiple .then statements, we can use the await keyword.
+    // The await keyword is like saying pause the execution of the code until getFruit returns a value
+    const a = await getFruit('pineapple');
+    const b = await getFruit('peach');
+    const c = await getFruit('strawberry');
+
+    //If we were to use .then chains, it would be difficult to use the promissed variables together
+    // await resolves that problem
+    // The code would look like this
+    /**
+      const makeSmoothie = () => {
+        let a;
+        return getFruit('pineapple')
+              .then (v=> {
+                v = a;
+                return getFruit('strawberry')
+              })
+              .then (v => v + a)
+      }
+
+    **/
+
+    return [a, b, c];
+
+    //Second note,
+    //The above makeSmoothie call is not ideal.
+    //When using async/await, avoid pausing the event loop when it does not need to be paused.
+    // Example
+    /*
+    try{
+      const a =  getFruit('pineapple');
+      const b =  getFruit('peach');
+      const c =  getFruit('strawberry');
+
+      const smoothie = await Promise.all([a, b, c]);
+      return smoothie;
+    } catch(err){
+      console.log(err);
+    }
+
+    */
+    // Promise.all allows all promises in the array to run concurrently, and allow the promise to be at that index in the array
+
+    //Another benefit of async/await is we can put all async functions in a try/catch block
+    //By using try/catch blocks, we can dictate how the user of this promise's code will flow if an error is thrown in this function
+    // ie in the catch block, if we return a value, the user's code will continue with the next .then call, but if we throw an error, the users .catch block will start executing
+    // TLDR : return is like ignoring the error and sending a new value, and throwing is stopping the .then chain and going to the .catch code
+
+
+  }
+
+  const printAsyncAwait = () => {
+    return makeSmoothie().then(console.log);
+  }
+
+  // 7.2 - Using async/await loop
+  const smoothieLoop = () => {
+    const fruits = ['pineapple', 'peach','strawberry', ];
+
+    const smoothie = fruits.map(v => getFruit(v));
+
+    const fruitLoop = async() => {
+      for await (const s of smoothie){
+        console.log(`Inside fruitLoop ${s}`);
+      }
+    }
+
+    fruitLoop(); 
+    return "hello";
+  }
 
 
 
+  // 7.3 - Using async/await condition
+  const fruitInspection = async() => {
+    if(await getFruit('peach') === 22){
+      return "You have a peach";
+    } else{ 
+      return "You do not have a peach";
+    }
+  }
 
-
+  const fruitIf = () => {
+     fruitInspection()
+     .then(v => console.log(v));
+  }
 
   return (
     <div className="App">
@@ -183,10 +303,19 @@ function App() {
       {/** 5.1 **/ }{(console.log(withTax))}
       {/** 5.2 **/ }{(console.log(highValue))}
       {/** 6.0 **/ }{(console.log(sumRandomAsyncNums()))}
+      {/** 7.0 **/ }{(console.log(testFetch()))}
+      {/** 7.1 **/ }{(console.log(printAsyncAwait()))}
+      {/** 7.2 **/ }{(console.log(smoothieLoop()))}
+      {/** 7.3 **/ }{(console.log(fruitIf()))}
       <h1>hello</h1>
     
     </div>
   );
 }
+/**
+  Key things to remember
+    - If you are going to have an await call inside a function, that function needs to be async
+    
+**/
 
 export default App;
