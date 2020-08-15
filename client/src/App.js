a475ebe93fbbc2c2d76f79326d22c38666578947
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-15 07:49:36 -0700

    Output array of object in a table

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,13 @@ function App() {
   //When doing a console log, putting the variable inside {} will log the variable name and the content (JSON format)
   // console.log({foo, bar, baz}) prints  {foo: {…}, bar: {…}, baz: {…}} ## The {...} can be expanded to view the variable
 
+  //Console log variables with common properties in a table format
+  // console.table([foo, bar, baz])
+
   return (
     <div className="App">
       {(console.log({foo, bar, baz}))} 
+      {(console.table([foo, bar, baz]))}
       <h1>hello</h1>
     
     </div>
