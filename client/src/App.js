e11412b436cff818c85d57278bd90f308b2191c7
Mitchell <mitchell.rian.smith@gmail.com>
2020-08-15 07:47:27 -0700

    Console logging objects

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+
+  
+  let foo = { name: 'tom', sex: 'm'};
+  let bar = { name: 'tam', sex: 'f'};
+  let baz = { name: 'tim', sex: 'm'};
+
+  //When doing a console log, putting the variable inside {} will log the variable name and the content (JSON format)
+  // console.log({foo, bar, baz}) prints  {foo: {…}, bar: {…}, baz: {…}} ## The {...} can be expanded to view the variable
+
   return (
     <div className="App">
+      {(console.log({foo, bar, baz}))} 
       <h1>hello</h1>
+    
     </div>
   );
 }
